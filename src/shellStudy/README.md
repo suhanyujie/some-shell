14e109cd1da69ecf3ce696c9f22e44b70f601914
suhanyujie <suhanyujie@qq.com>
2019-02-11 17:38:17 +0800

    shell-hello world

---
+++ src/shellStudy/README.md
@@ -0,0 +1,13 @@
+## study shell
+
+## Index
+* [准备工作](./workForReady/)
+
+
+
+
+## 参考资料
+* https://tinylab.gitbooks.io/shellbook/content/
+
+
+
---
+++ src/shellStudy/workForReady/hello.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+echo "hello world"
+type awk
+
+# ----------------------------------------
+## 知识点
+##### * 在shell脚本的第一行的`#!/bin/bash`，后加上`-v`参数，表示：要打印程序的源代码
+##### * 使用`type`可以查看各个命令的类型，例如：`type awk`
+
+
+## 注意事项
+##### * 表达式和运算符之间要有空格，例如`2+2`是不对的，要写成`2 + 2`
+
+## 参考资料
+##### * http://wiki.jikexueyuan.com/project/shell-tutorial/shell-brief-introduction.html
+##### * https://tinylab.gitbooks.io/shellbook/content/zh/chapters/01-chapter2.html
+# ----------------------------------------
