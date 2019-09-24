292696038fc437b55c6da35a0a2912b64bb3e56e
suhanyujie <suhanyujie@qq.com>
2019-09-24 10:09:39 +0800

    完善 创建数据库 并导入已有数据库

--- src/mysql/createDatabase.sh
+++ src/mysql/createDatabase.sh
@@ -4,11 +4,17 @@ DB_USER=root
 DB_PASS=123456
 DB_NAME=test_mysql5
 
-# 创建数据库
+# 创建数据库 并导入已有数据库
 function createDatabase() {
     mysql -u $DB_USER -p$DB_PASS << EOF 2> /dev/null
 CREATE DATABASE $DB_NAME
+USE $DB_NAME
+source /www/backup/ss-message_20190920_181114.sql
 EOF
 }
 
 createDatabase
+
+
+### 参考资料
+# - https://blog.csdn.net/wyl9527/article/details/72655277
